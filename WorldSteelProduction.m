3de1f276eaacfa28e0a661f00c87bb7713897bcf
Hlumelo Siyo <hlumelosiyo2@gmail.com>
2023-03-04 00:46:45 +0200

    Updated WorldSteelProduction.m file

--- WorldSteelProduction.m
+++ WorldSteelProduction.m
@@ -1,6 +1,6 @@
 Steel = xlsread('WorldSteelProduction.xlsx');
 plot(1:12,Steel(2:end,:))
-legend('2005','2006','2007','2008','2009','2010', ...
-    'Location','best')
+legend('2005','2006','2007','2008','2009','2010','Location','best')
 disp('Select the 2009 time when production is 100,000.');
-% [t,1] = ginput(1) % mouse click
+[t,l] = ginput(1) % mouse click
+fprintf('Level %6.1f occurs when %3.1f months into 2009.\n',l,t)
