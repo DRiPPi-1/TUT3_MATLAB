8070a98d956d6f6aabdc2292035a3587f08b13ed
Hlumelo Siyo <hlumelosiyo2@gmail.com>
2023-03-05 18:57:04 +0200

    Modified files
    changed the spacing and decimal places, also added prompt

--- Runge_kutta_Q4_bird_DE2.m
+++ Runge_kutta_Q4_bird_DE2.m
@@ -2,16 +2,17 @@ clearvars
 format long
 %Runge Kutta solution
 % total steps
-Total_steps=5;
+tsteps=input("Enter total steps: ");
+Total_steps=tsteps;
 % step size
 h=0.1;
 % counter
 n=1;
 x(n)=0;
 y(n)=2;
-fprintf('%2s %4s %4s %4s %4s %4s %8s\n','n','x','k1','k2','k3','k4','y')
- formatSpec = '%d %4.2f  %33.3f \n';    
-fprintf(formatSpec,n,x(n),y(n))
+fprintf('%s %s %5s %6s %6s %6s %5s\n','n','x','k1','k2','k3','k4','y')
+formatSpec = '%d %4.2f  %35.6f \n';    
+fprintf(formatSpec,n-1,x(n),y(n))
 % runge kutta steps
 for n = 1:Total_steps
     k1=f(x(n),y(n));
@@ -20,11 +21,11 @@ for n = 1:Total_steps
     k4=f(x(n)+h,y(n)+h*k3);
     y(n+1)=y(n)+h/6.0*(k1+2*k2+2*k3+k4);
     x(n+1)=x(n)+h;
-formatSpec = '%d %4.2f %6.4f %6.4f %6.4f %6.4f  %8.6f \n';  
-fprintf(formatSpec,n+1,x(n+1),k1,k2,k3,k4,y(n+1))
+    formatSpec = '%d %4.2f %6.4f %6.4f %6.4f %6.4f %5.6f \n';  
+    fprintf(formatSpec,n,x(n+1),k1,k2,k3,k4,y(n+1))
 end    
-%plot(x,y)
 
-function f = f(x,y)
-   f = y-x;
+%plot(x,y)
+function fx = f(x,y)
+   fx = y-x;
 end
